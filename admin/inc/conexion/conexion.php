a768c2f6c9f931bc3fc3bb21a399c7db693d69ce
CVz <carlosdavidvasquez7@gmail.com>
2018-11-01 17:29:00 -0600

    Archivo de conexion

---
+++ admin/inc/conexion/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$hostname_cn = "localhost";
+$database_cn = "scsiguatepeque";
+$username_cn = "root";
+$password_cn = "DexScorp";
+$db = mysqli_connect($hostname_cn,$username_cn,$password_cn,$database_cn);
+?>
